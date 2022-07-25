904262c379a4487dc32bd85ad3b9df8d3ab0546c
Enlith Zhou <enlith.zhou@ericsson.com>
2022-07-25 15:06:08 +0200

    mount gic on linux

--- mountHome.sh
+++ mountHome.sh
@@ -2,5 +2,23 @@
 \\vhub.rnd.ki.sw.ericsson.se\home\ezhoyux
 
 # mount gic home to ubuntu
-sudo mkdir /mnt/$USER_gichome
-sudo mount -t cifs -o rw,vers=3.0,user=$USER //seroisfile.sero.gic.ericsson.se/home/$USER /mnt/$USER_gichome
+GICHOME_FOLDER="$HOME"/"$USER"_gichome
+sudo mkdir -p $GICHOME_FOLDER
+sudo mount -t cifs -o rw,vers=3.0,user=$USER //seroisfile.sero.gic.ericsson.se/home/$USER $GICHOME_FOLDER
+
+
+
+## {add below lines to .profile file
+
+# if gichome folder is created, mount gic home
+GICHOME_FOLDER="$HOME"/"$USER"_gichome
+if [ -d "$GICHOME_FOLDER" ] ; then
+    if grep -qs $GICHOME_FOLDER /proc/mounts ; then
+        echo "GIC home is mounted on $GICHOME_FOLDER"
+    else
+        echo "Mounting GIC home, ctrl-c to cancel!!!"
+        sudo mount -t cifs -o rw,vers=3.0,user=$USER //seroisfile.sero.gic.ericsson.se/home/$USER $GICHOME_FOLDER
+    fi
+fi
+
+## }add below lines to .profile file
