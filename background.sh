7d65dc48527420ea006f24975548197d32bdb748
Enlith Zhou <enlith.zhou@ericsson.com>
2023-04-06 13:06:25 +0200

    increase cpu rate up to 1x%

--- background.sh
+++ background.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-# create background cpu occupy rate between 11% and 20%. Avoid statistic idle
+#
+# use on 4 cpus machine to make backgroud cpu rate up > 10%
+#
+stress --cpu 1 --timeout 60 &
+# Get the pid list from pgrep stress
+pids=$(pgrep stress)
 
-stress --cpu 1 --timeout 60 && cpulimit -l $((11 + $(od -An -N1 -i /dev/urandom) % 10)) -p $(pidof stress)
+limit=$((36 + $(od -An -N1 -i /dev/urandom) % 10))
+
+# Loop through each pid and echo it
+for pid in $pids; do
+  cpulimit -l $limit -p $pid &
+done
